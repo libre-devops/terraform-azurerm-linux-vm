1a2f9a78d8058c41aa7cee3fa15aae0318260faf
Craig Thacker <craigthackerx@gmail.com>
2022-04-18 18:42:42 +0100

    update asg

---
+++ output.tf
@@ -0,0 +1,48 @@
+output "vm_ids" {
+  description = "Virtual machine ids created."
+  value       = azurerm_linux_virtual_machine.linux_vm.*.id
+}
+
+output "vm_name" {
+  value = azurerm_linux_virtual_machine.linux_vm.*.name
+}
+
+output "vm_zones" {
+  description = "map with key `Virtual Machine Id`, value `list of the Availability Zone` which the Virtual Machine should be allocated in."
+  value       = zipmap(azurerm_linux_virtual_machine.linux_vm.*.id, azurerm_linux_virtual_machine.linux_vm.*.zone)
+}
+
+output "vm_identity" {
+  description = "map with key `Virtual Machine Id`, value `list of identity` created for the Virtual Machine."
+  value       = zipmap(azurerm_linux_virtual_machine.linux_vm.*.id, azurerm_linux_virtual_machine.linux_vm.*.identity)
+}
+
+output "vm_amount" {
+  description = "The amount of VMs passed to the vm_amount variable"
+  value       = var.vm_amount
+}
+
+output "nic_id" {
+  description = "The ID of the nics"
+  value       = azurerm_network_interface.nic.*.id
+}
+
+output "nic_ip_config_name" {
+  description = "The name of the IP Configurations"
+  value       = azurerm_network_interface.nic.*.ip_configuration
+}
+
+output "nic_ip_private_ip" {
+  description = "The private IP assigned to the NIC"
+  value       = azurerm_network_interface.nic.*.private_ip_address
+}
+
+output "asg_id" {
+  description = "The id of the ASG"
+  value       = azurerm_application_security_group.asg.name
+}
+
+output "asg_name" {
+  description = "The name of the ASG"
+  value       = azurerm_application_security_group.asg.name
+}
---
+++ vm.tf
@@ -0,0 +1,108 @@
+resource "azurerm_network_interface" "nic" {
+  count = var.vm_amount
+
+  name                = "nic-${var.vm_hostname}${format("%02d", count.index + 1)}"
+  resource_group_name = var.rg_name
+  location            = var.location
+
+  enable_accelerated_networking = var.enable_accelerated_networking
+
+  ip_configuration {
+    name                          = "nic-ipconfig-${var.vm_hostname}${format("%02d", count.index + 1)}"
+    primary                       = true
+    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = var.subnet_id
+  }
+  tags = var.tags
+
+  timeouts {
+    create = "5m"
+    delete = "10m"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "linux_vm" {
+
+  count                    = var.vm_amount
+  name                     = "${var.vm_hostname}${format("%02d", count.index + 1)}"
+  resource_group_name      = var.rg_name
+  location                 = var.location
+  network_interface_ids    = [azurerm_network_interface.nic[count.index].id]
+  license_type             = var.license_type
+  computer_name            = var.vm_hostname
+  admin_username           = var.admin_username
+  admin_password           = var.admin_password
+  size                     = var.vm_size
+  zone                     = var.availability_zone == "alternate" ? (count.index % 3) + 1 : null // Alternates zones for VMs in count, 1, 2 then 3. Use availability set if you want HA.
+
+  provision_vm_agent         = true
+  timezone                   = var.timezone
+
+  #checkov:skip=CKV_AZURE_151:Ensure Virtual Machine extensions are not installed
+  encryption_at_host_enabled = false
+
+  #checkov:skip=CKV_AZURE_50:Ensure Virtual Machine extensions are not installed
+  allow_extension_operations = true
+
+  dynamic "plan" {
+    for_each = toset(var.vm_plan != null ? ["fake"] : [])
+    content {
+      name      = lookup(var.vm_plan, "name", null)
+      product   = lookup(var.vm_plan, "product", null)
+      publisher = lookup(var.vm_plan, "publisher", null)
+    }
+  }
+
+  dynamic "admin_ssh_key" {
+    for_each = var.ssh_public_key != null ? ["fake"] : []
+    content {
+      public_key = var.ssh_public_key
+      username   = var.admin_username
+    }
+  }
+
+  source_image_reference {
+    publisher = var.vm_os_id == "" ? coalesce(var.vm_os_publisher, module.os_calculator.calculated_value_os_publisher) : ""
+    offer     = var.vm_os_id == "" ? coalesce(var.vm_os_offer, module.os_calculator.calculated_value_os_offer) : ""
+    sku       = var.vm_os_id == "" ? coalesce(var.vm_os_sku, module.os_calculator.calculated_value_os_sku) : ""
+    version   = var.vm_os_id == "" ? var.vm_os_version : ""
+  }
+
+  dynamic "identity" {
+    for_each = length(var.identity_ids) == 0 && var.identity_type == "SystemAssigned" ? [var.identity_type] : []
+    content {
+      type = var.identity_type
+    }
+  }
+
+  dynamic "identity" {
+    for_each = length(var.identity_ids) > 0 || var.identity_type == "UserAssigned" ? [var.identity_type] : []
+    content {
+      type         = var.identity_type
+      identity_ids = length(var.identity_ids) > 0 ? var.identity_ids : []
+    }
+  }
+
+  os_disk {
+    name                 = "osdisk-${var.vm_hostname}${format("%02d", count.index + 1)}"
+    caching              = "ReadWrite"
+    storage_account_type = var.storage_account_type
+    disk_size_gb         = var.vm_os_disk_size_gb
+  }
+
+  boot_diagnostics {
+    storage_account_uri = null // Use managed storage account
+  }
+
+  priority        = var.spot_instance ? "Spot" : "Regular"
+  max_bid_price   = var.spot_instance ? var.spot_instance_max_bid_price : null
+  eviction_policy = var.spot_instance ? var.spot_instance_eviction_policy : null
+
+  tags = var.tags
+}
+
+module "os_calculator" {
+  source = "registry.terraform.io/libre-devops/lnx-os-sku-calculator/azurerm"
+
+  vm_os_simple = var.vm_os_simple
+}
