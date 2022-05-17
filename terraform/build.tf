8ff364acdba80c2476462ad73c97dac863e9729b
Craig Thacker <craigthackerx@gmail.com>
2022-05-17 22:44:21 +0100

    Update

--- terraform/build.tf
+++ terraform/build.tf
@@ -7,3 +7,50 @@ module "rg" {
 
   #  lock_level = "CanNotDelete" // Do not set this value to skip lock
 }
+
+module "network" {
+  source = "registry.terraform.io/libre-devops/network/azurerm"
+
+  rg_name  = module.rg.rg_name // rg-ldo-euw-dev-build
+  location = module.rg.rg_location
+  tags     = local.tags
+
+  vnet_name     = "vnet-${var.short}-${var.loc}-${terraform.workspace}-01" // vnet-ldo-euw-dev-01
+  vnet_location = module.network.vnet_location
+
+  address_space   = ["10.0.0.0/16"]
+  subnet_prefixes = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names    = ["sn1-${module.network.vnet_name}", "sn2-${module.network.vnet_name}", "sn3-${module.network.vnet_name}"] //sn1-vnet-ldo-euw-dev-01
+  subnet_service_endpoints = {
+    "sn1-${module.network.vnet_name}" = ["Microsoft.Storage"]                   // Adds extra subnet endpoints to sn1-vnet-ldo-euw-dev-01
+    "sn2-${module.network.vnet_name}" = ["Microsoft.Storage", "Microsoft.Sql"], // Adds extra subnet endpoints to sn2-vnet-ldo-euw-dev-01
+    "sn3-${module.network.vnet_name}" = ["Microsoft.AzureActiveDirectory"]      // Adds extra subnet endpoints to sn3-vnet-ldo-euw-dev-01
+  }
+}
+
+
+module "lnx_vm_simple" {
+  source = "registry.terraform.io/libre-devops/linux-vm/azurerm"
+
+  rg_name  = module.rg.rg_name
+  location = module.rg.rg_location
+
+  vm_amount          = 1
+  vm_hostname        = "lnx${var.short}${var.loc}${terraform.workspace}"
+  vm_size            = "Standard_B2ms"
+  vm_os_simple       = "Ubuntu20.04"
+  vm_os_disk_size_gb = "127"
+
+  asg_name = "asg-${element(regexall("[a-z]+", element(module.lnx_vm_simple.vm_name, 0)), 0)}-${var.short}-${var.loc}-${terraform.workspace}-01" //asg-vmldoeuwdev-ldo-euw-dev-01 - Regex strips all numbers from string
+
+  admin_username = "LibreDevOpsAdmin"
+  admin_password = data.azurerm_key_vault_secret.mgmt_local_admin_pwd.value
+  ssh_public_key = data.azurerm_ssh_public_key.mgmt_ssh_key.public_key
+
+  subnet_id            = element(values(module.network.subnets_ids), 0)
+  availability_zone    = "alternate"
+  storage_account_type = "Standard_LRS"
+  identity_type        = "SystemAssigned"
+
+  tags = module.rg.rg_tags
+}
